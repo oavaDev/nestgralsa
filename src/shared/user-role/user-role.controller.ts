10f4115c40c58e28d1da56d1475fc9c13a7a831c
Omar Vega <omarvega598@gmail.com>
2024-10-03 12:28:15 -0500

    Feat(Response Entity(UserRole)): Add standard as response entity on controllers

--- src/shared/user-role/user-role.controller.ts
+++ src/shared/user-role/user-role.controller.ts
@@ -1,12 +1,17 @@
 import {Controller, Get} from '@nestjs/common';
 import { UserRoleService } from './user-role.service';
 import {ApiTags} from "@nestjs/swagger";
+import {createResponse} from "../../utils/shared/response.util";
 @ApiTags('user-role')
 @Controller('user-role')
 export class UserRoleController {
   constructor(private readonly userRoleService: UserRoleService) {}
   @Get()
   getAllUserRoles() {
-    return this.userRoleService.getAllUserRoles();
+    try {
+      return createResponse(this.userRoleService.getAllUserRoles(), "Roles de usuario obtenidos con éxito", 200);
+    }catch (e) {
+      return createResponse([], "Error al obtener los roles de usuario", 500);
+    }
   }
 }
