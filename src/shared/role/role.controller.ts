573512e88f5746510a5164fc4881f7c707ecfe65
Omar Vega <omarvega598@gmail.com>
2024-10-03 11:28:49 -0500

    Feat(Response Entity(Role)): Add standard as response entity on controllers

--- src/shared/role/role.controller.ts
+++ src/shared/role/role.controller.ts
@@ -3,16 +3,19 @@ import { RoleService } from './role.service';
 import {RoleEntity} from "./entities/role.entity";
 import {CreateRoleDto} from "./dto/create-role.dto";
 import {ApiTags} from "@nestjs/swagger";
+import {createResponse} from "../../utils/shared/response.util";
+import {ResponseEntity} from "../entity/response.entity";
 @ApiTags('role')
 @Controller('role')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
   @Get()
-  async getAllRoles() {
-      return await this.roleService.getAllRoles();
+  async getAllRoles(): Promise<ResponseEntity> {
+      return createResponse(await this.roleService.getAllRoles(), "Roles encontrados", 200);
   }
   @Post()
-  async createRole(@Body() createRoleDto:CreateRoleDto): Promise<RoleEntity> {
-    return await this.roleService.create(createRoleDto);
+  async createRole(@Body() createRoleDto:CreateRoleDto): Promise<ResponseEntity> {
+    const data = await this.roleService.create(createRoleDto);
+    return createResponse(data, "Rol creado con éxito", 201);
   }
 }
